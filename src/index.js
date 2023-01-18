14b64224407575e167b34462db4909f2e3bfc0ff
bdayman <daymanblake@gmail.com>
2023-01-18 13:04:54 -0800

    Improvement 1 & 2
    Displayed the location for each move in the format (col, row) in the move history list. Bolded the current selected item in the move list. Added comments for clarity.

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// Returns a  clickable square
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={"square"} onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
   
   class Board extends React.Component {
+
+    // Renders a  clickable square
     renderSquare(i) {
       return (
         <Square 
           value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}  
+          onClick={() => this.props.onClick(i)}
           />
       );
     }
-  
+    
+    // Renders the tic tac toe board
     render() {
       return (
         <div>
@@ -50,15 +54,33 @@ function Square(props) {
       this.state = {
         history: [{
           squares: Array(9).fill(null),
+          location: null
         }],
         stepNumber: 0,
         xIsNext: true,
+        selectedMove: null,
       };
     }
+
+    // game handles the square that was clicked and updates its value
     handleClick(i){
+
+      const locations = [
+        [1,1],
+        [2,1],
+        [3,1],
+        [1,2],
+        [2,2],
+        [3,2],
+        [1,3],
+        [2,3],
+        [3,3]
+      ];
+
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
+
       if (calculateWinner(squares) || squares[i]) {
         return;
       }
@@ -66,6 +88,7 @@ function Square(props) {
       this.setState({
         history: history.concat([{
           squares: squares,
+          location: locations[i]
         }]),
         stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
@@ -76,6 +99,7 @@ function Square(props) {
       this.setState({
         stepNumber: step,
         xIsNext: (step % 2) === 0,
+        selectedMove: step,
       });
     }
 
@@ -86,15 +110,18 @@ function Square(props) {
 
       const moves = history.map((step, move) => {
         const desc = move ?
-          'Go to move #' + move :
+          'Go to move # ' + move + ' @ ' + history[move].location :
           'Go to game start';
         return (
           <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            <button className={move === this.state.selectedMove ? 'selectedMove' : 'button'} 
+              onClick={() => this.jumpTo(move)}>{desc}
+            </button>
           </li>
         );
       });
 
+      // game renders the game status
       let status;
       if(winner){
         status = 'Winner: ' + winner;
@@ -102,6 +129,7 @@ function Square(props) {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
 
+      // game returns the structure of tic tac toe
       return (
         <div className="game">
           <div className="game-board">
@@ -125,6 +153,8 @@ function Square(props) {
   root.render(<Game />);
   
   function calculateWinner(squares) {
+
+    // winning lines
     const lines = [
       [0,1,2],
       [3,4,5],
@@ -135,6 +165,7 @@ function Square(props) {
       [0,4,8],
       [2,4,6],
     ];
+
     for (let i=0; i<lines.length; i++){
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
